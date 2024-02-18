088a6680a16e2c541e7c8f754619927db1abc20a
KarloSiric <ksiric99@gmail.com>
2024-02-18 10:40:53 +0100

    init commit

---
+++ Codingbat_practice/Exercise1/Exercise1/src/Test123.java
@@ -0,0 +1,15 @@
+public class Test123 {
+
+    /*
+     * FOR TESTING PURPOSES ONLY 
+     * NOT PART OF AN EXERCISES!
+     */
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("For testing purposes only");
+        System.out.println("I am testing for future exercises");
+        System.out.println("THIS IS NOT PART OF THE CODINGBAT EXERCISES");
+        
+    }
+}
