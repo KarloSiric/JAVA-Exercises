d0a28e2b1434acacd72cd7f61bdf65c80c30fd21
KarloSiric <ksiric99@gmail.com>
2024-02-18 10:24:12 +0100

    2nd commit

---
+++ Codingbat_practice/Exercise1/test123.java
@@ -0,0 +1,18 @@
+public class test123 {
+
+    /*
+     * JUST FOR TESTING PURPOSES
+     * NOT PART OF EXERCISES RELATED TO CODINGBAT.COM
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("This is just for testing purposes!!");
+        System.out.println("This is not part of an exercises!!");
+
+
+
+
+    }
+    
+}
